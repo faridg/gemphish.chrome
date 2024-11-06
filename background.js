64e39f1d388e6bc0f6cb2af6eadc165a16188995
faridg <farid.gassanov@gmail.com>
2024-11-06 16:43:22 +0400

    option to trigger sidepanel from context menu

--- background.js
+++ background.js
@@ -1,1 +1,15 @@
 chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+      id: "openSidePanel",
+      title: "Open Side Panel",
+      contexts: ["all"]
+    });
+  });
+
+  chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === "openSidePanel") {
+      chrome.sidePanel.open({ windowId: tab.windowId });
+    }
+  });
